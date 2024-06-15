e5ad06ce31a9f31f31226bfabbdd3926fa0f0973
Harsh Yadav <115269033+Harsh4766@users.noreply.github.com>
2024-06-15 20:58:37 +0530

    Commit

---
+++ index.js
@@ -0,0 +1,44 @@
+var tablinks=document.getElementsByClassName('tab-links');
+var tabcontents=document.getElementsByClassName('tab-content');
+
+function opentab(tabname)
+{
+    for(tablink of tablinks)
+        {
+            tablink.classList.remove('active-link');
+        }
+
+    for(tabcontent of tabcontents)
+        {
+            tabcontent.classList.remove('active-tab');
+        }
+    
+    event.currentTarget.classList.add('active-link');
+    document.getElementById(tabname).classList.add('active-tab');
+}
+
+var menu=document.getElementById('sidemenu');
+
+function openmenu(){
+    menu.style.right="0px";
+}
+
+function closemenu(){
+    menu.style.right="-250px";
+}
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxzkTpnzyMhGIiuunaHPGuUKdjVlpv_KFKL_45sEhMTq7EW_UbvmyHH4u4rOHlKx1smJg/exec';
+  const form = document.forms['submit-to-google-sheet']
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => console.log('Success!', response))
+      .catch(error => console.error('Error!', error.message))
+
+      document.getElementById('Name').value="";
+      document.getElementById('Email').value="";
+      document.getElementById('Message').value="";
+
+      alert("Your message sent successfully");
+  })
